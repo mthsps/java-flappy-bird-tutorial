6d9fa22924c58f86db936b74308a21271a5100b8
Matheus <dmatheusps@gmail.com>
2022-04-06 14:34:43 -0300

    Refactoring code to follow Oracle Code Conventions

--- flappyBird/FlappyBird.java
+++ flappyBird/FlappyBird.java
@@ -236,26 +236,34 @@ public class FlappyBird implements ActionListener, MouseListener, KeyListener {
 		
 	}
 	
+	@Override
 	public void mouseClicked(MouseEvent e) {
 		jump();
 	}
 	
+	@Override
 	public void keyReleased(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
 			jump();
 		}
 	}
 	
+	@Override
 	public void mousePressed(MouseEvent e) {}
 	
+	@Override
 	public void mouseReleased(MouseEvent e) {}
 	
+	@Override
 	public void mouseEntered(MouseEvent e) {}
 	
+	@Override
 	public void mouseExited(MouseEvent e) {}
 	
+	@Override
 	public void keyTyped(KeyEvent e) {}
 	
+	@Override
 	public void keyPressed(KeyEvent e) {}
 
 }
